c071f80a4abece9bc71edd502e6c9dda3b99d2a2
Michał <michalserej@gmail.com>
2016-09-07 06:55:22 +0200

    Zmiana katalogu dla biblioteki C3D.dll

---
+++ C3D.NET-master/C3D/C3DProcessorType.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace C3D
+{
+    /// <summary>
+    /// 处理器类型
+    /// </summary>
+    public enum C3DProcessorType : byte
+    {
+        /// <summary>
+        /// Intel处理器
+        /// </summary>
+        Intel = 84,
+
+        /// <summary>
+        /// DEC处理器
+        /// </summary>
+        DEC = 85,
+
+        /// <summary>
+        /// MIPS处理器
+        /// </summary>
+        MIPS = 86
+    }
+}
